b2189692053c876dee2512b29465c73b73483d9e
Remo <dev@remofritzsche.com>
2024-04-24 16:05:10 +0000

    Set up

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
+// WELCOME
 
 ?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
---
+++ tasks.md
@@ -0,0 +1,2 @@
+1. Personen aus CSV-Datei `people.csv` einlesen, Daten bereinige (Whitespaces etc.)
+2. Personen in custom Objekte konvertieren, dabei `name` in `firstname` und `lastname` aufteilen
